445c348b1e18fb342b1dca6dad051d0dd8cbceb2
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2024-11-24 19:13:41 -0300

    test

--- server.js
+++ server.js
@@ -8,18 +8,25 @@ dotenv.config();
 const path = require('path');
 
 const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// Conexão com o MongoDB
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Conectado ao MongoDB'))
     .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
 
+// Rotas
 app.use('/api/cards', cardRoutes);
 app.use('/auth', authRoutes);
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
 
+// Iniciando o servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
